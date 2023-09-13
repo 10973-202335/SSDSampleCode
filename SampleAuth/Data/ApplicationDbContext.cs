0aa0cde75b9d643405d06d798c599289b57da711
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2023-09-12 22:53:52 -0400

    Add project files.

---
+++ SampleAuth/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SampleAuth.Models;
+
+namespace SampleAuth.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        //Database Tables
+    }
+}
---
+++ SampleAuth/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SampleAuth.Models
+{
+public class ApplicationUser : IdentityUser
+{
+[Required,Display(Name = "First Name")]
+public string FirstName { get; set; } = String.Empty;
+#nullable disable
+[Required, Display(Name = "Last Name")]
+public string LastName { get; set; }
+}
+}
