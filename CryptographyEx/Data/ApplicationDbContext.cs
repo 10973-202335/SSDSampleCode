e49227327ff4a4fb448ef8a7afc5675d00292466
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2023-10-31 13:43:37 -0400

    Cryptography - Part 1

---
+++ CryptographyEx/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CryptographyEx.Models;
+
+namespace CryptographyEx.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CryptographyEx.Models.Password>? Password { get; set; }
+    }
+}
