fd4520e7b654943180cba6c04f3055583d5a979b
Mohamed Elliethy <mohamed.elliethy@mohawkcollege.ca>
2023-11-07 22:43:09 -0500

    Cryptography - Part 4

--- CryptographyEx/Data/ApplicationDbContext.cs
+++ CryptographyEx/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace CryptographyEx.Data
         }
         public DbSet<CryptographyEx.Models.Password>? Password { get; set; }
         public DbSet<CryptographyEx.Models.BankAccount>? BankAccount { get; set; }
+        public DbSet<CryptographyEx.Models.BankAccountData>? BankAccountData { get; set; }
     }
 }
